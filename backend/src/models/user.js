63502630014ca454a18337f567fba37714027bd2
lunaSantosDev <lunasantos.dev@gmail.com>
2025-12-04 22:26:33 -0300

    feat: creat user.js

---
+++ backend/src/models/user.js
@@ -0,0 +1,30 @@
+import { DataTypes } from "sequelize";
+
+const User = (sequelize) => {
+  return sequelize.define("User", {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    photoUrl: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+  });
+};
+
+export default User;
